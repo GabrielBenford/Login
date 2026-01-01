a312d74ce5873b92d7b1720c4aa6607447e0650a
GabrielBenford <loureirogabriel24@gmail.com>
2026-01-01 19:58:40 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -4,10 +4,9 @@ from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import random
+import random 
 class Login:
     usernames=['locked_out_user','standard_user','problem_user','performance_glitch_user','error_user','visual_user']
-    passwords=['secret_sauce','Eva']
     def __init__(self,driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
@@ -17,8 +16,7 @@ class Login:
     def username_password(self,):
         random_usernames=random.choice(self.usernames)
         self.wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys(random_usernames)
-        random_passwords=random.choice(self.passwords)
-        self.wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys(random_passwords)
+        self.wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys('secret_sauce')
     def submit_login(self):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,".submit-button.btn_action"))).click()
     def adding_cart(self):
