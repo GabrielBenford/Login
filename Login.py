5ef4068aba5225a53d6525853c7cc1e8b7e27ce7
GabrielBenford <loureirogabriel24@gmail.com>
2026-01-01 15:27:02 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -19,25 +19,10 @@ class Login:
         self.wait.until(EC.visibility_of_element_located((By.ID, "user-name"))).send_keys(random_usernames)
         random_passwords=random.choice(self.passwords)
         self.wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys(random_passwords)
-    def making_login(self):
+    def submit_login(self):
         self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,".submit-button.btn_action"))).click()
     def adding_cart(self):
         shop=self.wait.until(EC.element_to_be_clickable((By.ID,"add-to-cart-test.allthethings()-t-shirt-(red)")))
         self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});",shop)
-
-def main():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    try:
-        login = Login(driver)
-        login.open_website()
-        login.username_password()
-        login.making_login()
-        login.adding_cart()
-    finally:
-        driver.quit()
-if __name__ == '__main__':
-    main()
-
 
 
